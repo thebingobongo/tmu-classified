1f5ec6deed6283e5408d021c24c8c416749613c4
Bingo <55219554+thebingobongo@users.noreply.github.com>
2024-04-03 17:32:47 -0400

    changed app to be vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import './App.css';
+import logo from './assets/TMU-rgb.png';
+import Home from './Home';
+import RegisterAcc from './register_acc';
+import SignIn from './sign_in';
+import PostAd from './post_ad';
+import ItemsWanted from './items_wanted';
+import ItemsSale from './items_sale';
+import Services from './services';
+import UserProfile from './user_profile';
+import SearchResults from './search_results';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="entire_page">
+      <hr className='top_bar_decoration'/>
+        <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/register_acc" element={<RegisterAcc />} />
+              <Route path="/sign_in" element={<SignIn />} />
+              <Route path="/post_ad" element={<PostAd />} />
+              <Route path="/items_wanted" element={<ItemsWanted />} />
+              <Route path="/items_sale" element={<ItemsSale />} />
+              <Route path="/services" element={<Services />} />
+              <Route path="/user_profile" element={<UserProfile />} />
+              <Route path="/search_results" element={<SearchResults />} />
+            </Routes>
+        </BrowserRouter>
+        <br></br>
+        <hr className='bottom_bar_decoration'/>
+        <p className='contact_us'>Contact us at @tmuaddwebsite@gmail.com</p>
+        <br></br>
+    </div>
+  );
 }
-
-export default App
+export default App;
