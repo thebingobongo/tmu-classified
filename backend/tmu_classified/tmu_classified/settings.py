662f8a22544886f19f0252e9beabbe19282cecb7
Bingo <55219554+thebingobongo@users.noreply.github.com>
2024-04-04 00:59:59 -0400

    update settings.py to accomodate media files

--- backend/tmu_classified/tmu_classified/settings.py
+++ backend/tmu_classified/tmu_classified/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media files (User uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
