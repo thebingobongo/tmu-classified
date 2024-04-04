72db7e5561f0cc05e8850a0fa8b3c33ab9344fe6
Bingo <55219554+thebingobongo@users.noreply.github.com>
2024-04-04 01:01:54 -0400

    register models in admin site

--- backend/tmu_classified/tmu_classified_app/admin.py
+++ backend/tmu_classified/tmu_classified_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Ad, UserAdRelation
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Ad)
+admin.site.register(UserAdRelation)
