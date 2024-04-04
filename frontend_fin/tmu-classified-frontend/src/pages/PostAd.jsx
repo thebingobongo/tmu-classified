6f07776f5744e959a16e6b14b645f5c261a2d117
Omar <127859769+OmarTMU@users.noreply.github.com>
2024-04-04 19:42:20 -0400

    css

--- frontend_fin/tmu-classified-frontend/src/pages/PostAd.jsx
+++ frontend_fin/tmu-classified-frontend/src/pages/PostAd.jsx
@@ -8,7 +8,6 @@ const PostAd = () => {
     return (
         <div className='post-ad-page'>
             < Header />
-            <div className='post-ad-body'>
             <div className="post-ad-container">
                 <h2 className="signin-title">Post an Ad</h2>
                 <form className="signin-form">
@@ -16,9 +15,23 @@ const PostAd = () => {
                     <textarea placeholder="Description" className="signin-input" />
                     
                     <div className="signin-input">
-                        <label><input type="radio" name="category" value="Items for Sale" /> Items for Sale</label>
-                        <label><input type="radio" name="category" value="Items Wanted" /> Items Wanted</label>
-                        <label><input type="radio" name="category" value="Academic Services" /> Academic Services</label>
+                        <label>
+                            <input type="radio" name="category" value="Items for Sale" />
+                            <span className='radio_label0'></span>
+                            <span className='option_text'>Items for Sale</span>
+                        </label>
+
+                        <label>
+                            <input type="radio" name="category" value="Items Wanted" />
+                            <span className='radio_label0'></span>
+                            <span className='option_text'>Items Wanted</span>
+                        </label>
+                        
+                        <label>
+                            <input type="radio" name="category" value="Academic Services" />
+                            <span className='radio_label0'></span>
+                            <span className='option_text'>Academic Services</span>
+                        </label>
                     </div>
                     <select className="signin-input">
                         <option value="">Select Category</option>
@@ -41,7 +54,6 @@ const PostAd = () => {
                     <input type="file" className="signin-input" />
                     <button type="submit" className="signin-button">Post Ad</button>
                 </form>
-            </div>
             </div>
 
         </div>
