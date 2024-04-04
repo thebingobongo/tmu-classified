f378a70f65c0b309066d51dff50e515aef33777e
Bingo <55219554+thebingobongo@users.noreply.github.com>
2024-04-04 15:40:51 -0400

    Add profile to header for demo

--- frontend_fin/tmu-classified-frontend/src/components/Header/Header.jsx
+++ frontend_fin/tmu-classified-frontend/src/components/Header/Header.jsx
@@ -14,8 +14,8 @@ const Header = () => {
             <div className="header-button-group">
                 <Link className='header-button' to='/Signin'>Login</Link> <span></span>
                 <Link className='header-button' to='/Register' >Sign Up</Link> <span></span>
+                <Link className='header-button' to='/Profile'>Profile</Link> <span></span>
                 <Link className='big-button header-button' to='/PostAd'>Post Ad!</Link> <span></span>
-                <Link className='header-button-profile' to='/Profile'>Profile</Link> <span></span>
             </div>
         </div>
       </div>
