e02c90f517ed48dc5dcd878173f444041a3e226e
Omar <127859769+OmarTMU@users.noreply.github.com>
2024-04-04 18:51:30 -0400

    css

--- frontend_fin/tmu-classified-frontend/src/components/Header/Header.jsx
+++ frontend_fin/tmu-classified-frontend/src/components/Header/Header.jsx
@@ -6,19 +6,17 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-      <div className='header-section'>
         <div className="header-container">
-            <Link to='/'>
+            <Link className='header-logo-link' to='/'>
                 <img className="header-logo" src={logo} alt="Logo" />
             </Link>
             <div className="header-button-group">
-                <Link className='header-button' to='/Signin'>Login</Link> <span></span>
-                <Link className='header-button' to='/Register' >Sign Up</Link> <span></span>
-                <Link className='big-button header-button' to='/PostAd'>Post Ad!</Link> <span></span>
-                <Link className='header-button-profile' to='/Profile'>Profile</Link> <span></span>
+                <Link className='link header-button' to='/Signin'>Login</Link> <span></span>
+                <Link className='link header-button' to='/Register' >Sign Up</Link> <span></span>
+                <Link className='link big-button header-button' to='/PostAd'>Post Ad!</Link> <span></span>
+                <Link className='link header-button-profile' to='/Profile'>Profile</Link> <span></span>
             </div>
         </div>
-      </div>
     );
 };
 
